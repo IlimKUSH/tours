c3ca1c2ddc2a5e7468c2bcc433c12b89e2010f76
ilimKUSH <ilimKUSH>
2023-08-15 11:01:49 +0600

    added prettier and libs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "prettier"],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "camelcase": "error",
+    "spaced-comment": "error",
+    "quotes": ["error", "single"],
+    "no-duplicate-imports": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
